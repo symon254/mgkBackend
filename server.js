5bf9c9a0ef6071025d41590bdfd3ca7255bd8564
symon254 <79975499+symon254@users.noreply.github.com>
2023-03-10 12:56:17 +0300

    second

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const colors = require("colors");
 const dotevn = require("dotenv").config();
@@ -5,14 +6,29 @@ const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDb = require("./Config/db");
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
+
 connectDb();
 const app = express();
+
+//use cors
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/movies", require("./Routes/movieRoutes"));
 app.use("/api/users", require("./Routes/userRoutes"));
+
+//serve frontend
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../mgk/build")));
+    app.get("*", (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, "../", "mgk", "build", "index.html")
+        )
+    );
+}
 
 app.use(errorHandler);
 
